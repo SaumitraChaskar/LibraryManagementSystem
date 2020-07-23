aff81d6ab4e59a24356b4f63e3ab29f84b46f530
SaumitraChaskar <42095224+SaumitraChaskar@users.noreply.github.com>
2020-07-23 14:31:36 +0530

    Update ConnectionSsetterClass.java

--- src/application/ConnectionSsetterClass.java
+++ src/application/ConnectionSsetterClass.java
@@ -7,7 +7,7 @@ public class ConnectionSsetterClass {
 	
 	public Connection CreateConnection() throws SQLException {
 	String url ="jdbc:mysql://localhost:3306/test_db?useSSL=false";  	
-	Connection conn = DriverManager.getConnection(url,"root","dada9946");
+	Connection conn = DriverManager.getConnection(url,"root","---");
 	return conn;
 	}
 }
